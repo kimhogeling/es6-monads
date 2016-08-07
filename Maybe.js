695270f5cab25bd688e14e700c55f319445593b2
Kim Hogeling <kimhogeling@gmail.com>
2016-08-07 08:54:11 +0200

    add comments

--- Maybe.js
+++ Maybe.js
@@ -1,5 +1,17 @@
+/**
+ * Take a given value and check if its value is something (not just falsy, because of 0 and empty string)
+ *
+ * @param {Any} a The var to check
+ * @returns {Boolean} True if it is neither null, undefined or NaN
+ */
 const isSomething = a => (a !== null && a !== undefined && typeof a !== 'number') || (typeof a === 'number' && !isNaN(a))
 
+/**
+ * The maybe takes a value, validates it, wrappes it in a Just or a Nothing and returns that
+ * 
+ * @param {Any} Some value
+ * @returns {Just|Nothing} The wrapped value
+ */
 class Maybe {
     constructor(a) {
         return isSomething(a) ? new Just(a) : new Nothing()
@@ -15,6 +27,7 @@ class Just {
         return this.val
     }
 
+    // I named the Left's and Right's functor .fmap instead of .map to prevent confusion with Array.prototype.map
     fmap(fn) {
         return new Just(fn(this.val))
     }
